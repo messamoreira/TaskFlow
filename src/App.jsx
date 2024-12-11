16c9a2b567e5e425ce079064a976caef59e99a16
Messa <bmf741@gmail.com>
2024-12-11 18:35:42 -0300

    feature/created-ontaskclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
-import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar springboot",
+      description: "Estudar java pelo curso do Neilão.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar react",
+      description: "Estudar com a doc.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar single page app",
+      description: "estudar pela doc tailwind.",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // need uptade task
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      // dont need uptade
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
-    <div>
-      <h1 className="text-red-500">Task Flow</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
     </div>
   );
 }
